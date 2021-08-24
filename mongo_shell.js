6ea9a25220ab2136884b0c198bb61d6393b098c3
ababinov <alex.babinoff.cheat@gmail.com>
2021-08-24 14:51:09 +0300

    mongoshell parsing

--- mongo_shell.js
+++ mongo_shell.js
@@ -15,7 +15,7 @@
 //     ],
 //     { allowDiskUse: true }
 //   )
-  
+
 
 
 //   use("userstatistics");
@@ -146,22 +146,28 @@
 /// OUT найти, посмотреть и удалить
 
 use('userstatistics');
-db.sessions.find( { $where: function() {
-	if (this.commonTime - this.idlingTime > 400)
-		return this.commonTime
-} } ).count()
+db.sessions1147.find( { $where: function() {
+	if (this.commonTime - this.idlingTime > 200)
+		if (this.)
+		return this
+} } )
 
 use('userstatistics');
-db.sessions.remove( { $where: function() {
+db.sessions1147.remove( { $where: function() {
 if (this.commonTime - this.idlingTime > 400)
 	return this.commonTime
 } } );
 
 
 use('userstatistics');
-db.sessions.find( { $where: function() {
+db.sessions1147.find( { $where: function() {
 	return (this.centralPath.includes('id-1147'))
-} } );
+} } ).count()
+
+use('userstatistics');
+db.sessions1147.find( { $where: function() {
+	return (this.centralPath.includes('id-1147'))
+} } ).count()
 
 
 //    use('userstatistics');
@@ -179,3 +185,171 @@ db.sessions.find( { $where: function() {
 { $where : function() {
 	return (this.centralPath.includes('id-1147'))
 } }
+
+use('userstatistics');
+db.analyzing.find().forEach(
+    function (x) {
+        value = x.centralPath;
+        if(value != null){
+          db.analyzing.deleteOne({ "_id" : x._id })
+        }
+    }
+);
+
+use('userstatistics');
+db.sessions.find().forEach(
+    function (x) {
+        value = x.centralPath;
+        if(value.includes('id-1091')){
+          db.sessions1091.insertOne(x)
+        }
+    }
+);
+
+use('userstatistics');
+db.sessions1147.find().forEach(
+    function (sesion) {
+        elements = sesion.elementsCreated;
+		if (elements != null){
+			elements.forEach( 
+				function (element)
+				{
+					return element.nameCategory == "Воздуховоды"
+				}
+			)
+		}
+    }).count()
+
+
+	use('userstatistics');
+	db.sessions1147.find( { $where: function() {
+		let test = []
+		elements = this.elementsCreated;
+		if (elements != null){
+			for (const element in elements) {
+				if (element.nameCategory == "Воздуховоды")
+				{
+					test.push(element)
+				}
+			}
+		}
+		return test
+	} } )
+
+
+	use('userstatistics');
+	db.sessions1147.find( { $where: function() {
+		let test = []
+		elements = this.elementsCreated;
+		if (elements != null){
+			elements.forEach(
+				function (element)
+				{
+					return element.nameCategory == "Воздуховоды"
+				}
+			)
+		}
+	} } ).count()
+
+
+	use('userstatistics');
+	db.sessions1147.find( { $where: function() {
+		const test = this.elementsCreated
+		if (test != null){
+			test.forEach(function (element)
+			{
+				if (element.nameCategory == "Воздуховоды")
+					return element.nameCategory
+			})
+		}
+	}} ).count()
+
+	use('userstatistics');
+	test_elements = []
+	users = new Set();
+	category_name = "Воздухораспределители"
+	all_time = 0
+	elements_count = 0
+	db.sessions1147.find().forEach(
+		function (x) {
+			test = x.elementsCreated
+			if (test != null){
+				test.forEach(function (element)
+				{
+					if (element.nameCategory == category_name)
+						{
+							elements_count += element.count
+							users.add(x.userAdName)
+							all_time += x.commonTime - x.idlingTime
+						}
+				})
+			}
+		}
+	)
+	console.log({all_time: all_time})
+	console.log({elements_count: elements_count})
+	db.analyzing.insertOne({category_name: category_name, users: Array.from(users), all_time: all_time, elements_count: elements_count})
+
+	departament_name = "Конструкторский отдел"
+	departament_name = "Архитектурный отдел"
+
+	use('userstatistics');
+	test_elements = []
+	users = new Set();
+	category_name = "Кабельные лотки"
+	departament_name = "Отдел инновационных разработок"
+	all_time = 0
+	elements_count = 0
+	db.sessions1147.find({ departament: departament_name }).forEach(
+		function (x) {
+			test = x.elementsCreated
+			if (test != null){
+				test.forEach(function (element)
+				{
+					if (element.nameCategory == category_name)
+						{
+							elements_count += element.count
+							users.add(x.userAdName)
+							all_time += x.commonTime - x.idlingTime
+						}
+				})
+			}
+		}
+	)
+	console.log({all_time: all_time})
+	console.log({elements_count: elements_count})
+	db.analyzing.insertOne({departament_name: departament_name, category_name: category_name, users: Array.from(users), all_time: all_time, elements_count: elements_count})
+
+
+
+	use('userstatistics');
+	departaments_names = ["Архитектурный отдел", "Конструкторский отдел", 
+	"Отдел систем отопления и вентиляции", "Отдел информационного моделирования", 
+	"Отдел систем водоснабжения и водоотведения", "Отдел инновационных разработок", 
+	"Отдел систем электроснабжения"]
+	test_elements = []
+	users = new Set();
+	category_name = "Листы"
+	departaments_names.forEach(function (departament_name){
+		all_time = 0
+		elements_count = 0
+		db.sessions1091.find({ departament: departament_name }).forEach(
+			function (x) {
+				test = x.elementsCreated
+				if (test != null){
+					test.forEach(function (element)
+					{
+						if (element.nameCategory == category_name)
+							{
+								elements_count += element.count
+								users.add(x.userAdName)
+								all_time += x.commonTime - x.idlingTime
+							}
+					})
+				}
+			}
+		)
+		console.log({all_time: all_time})
+		console.log({elements_count: elements_count})
+		db.analyzing1091.insertOne({departament_name: departament_name, category_name: category_name, users: Array.from(users), all_time: all_time, elements_count: elements_count})
+	})
